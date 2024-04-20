f66ff036b180a9f8b6ca604ae75bf323bead934a
Kenny <kennyrobstad@gmail.com>
2024-04-20 11:58:15 -0600

    added accordian js adjusted css to ensure mobile responsivness

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionBtns = document.querySelectorAll('.accordion-btn');
 
+    accordionBtns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            const content = this.nextElementSibling;
+            content.style.display = content.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+
+    // Update copyright year once DOMContentLoaded
+    var currentYear = new Date().getFullYear();
+    document.getElementById('copyright-year').textContent = currentYear;
+});
 
